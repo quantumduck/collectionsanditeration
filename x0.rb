9baf593de254f7f80f42c600b8d20ac04b5b1592
quantumduck <quantum.duck@hotmail.com>
2017-03-28 16:04:42 -0400

    exercise 0

---
+++ x0.rb
@@ -0,0 +1,25 @@
+myarray = ["blue", "red", "green", 32, 35, 27, 23, false, true, false, false, true,
+            "Drake", "Ed Sheeran", "Bruno Mars", :blue, :red, :green
+          ]
+
+myhash = {"homoblivious"     => "Not having the ability to recognize"\
+                                "homosexuals as homosexuals; a lack of gaydar.",
+          "Boobs Mcgee"      => "The nickname given to the girl inthe room"\
+                                "flaunting her chest the most.",
+          "Baklol"           => "A Baklol is a person who is supposed to be"\
+                                "stupid. It is a Bihari word, generally used"\
+                                "by the people of Bihar and Eastern Uttar"\
+                                "Pradesh in India.",
+          "The Wizard of Oz" => 1939,
+          "Citizen Kane"     => 1941,
+          "The Third Man"    => 1949,
+          "Shanghai"         => 24_256_800,
+          "Karachi"          => 23_500_000,
+          "Delhi"            => 16_787_941,
+          "Katie"            => 35,
+          "Mark"             => 27,
+          "Lauren"           => 23,
+         }
+
+puts myarray
+puts myhash
