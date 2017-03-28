acc3970a087188c9a27ffd5d5850e6a51d25d8c2
quantumduck <quantum.duck@hotmail.com>
2017-03-28 16:21:37 -0400

    exercise 1

---
+++ x1.rb
@@ -0,0 +1,33 @@
+colours = ["blue", "red", "green"]
+ages = [32, 35, 27, 23]
+flips = [false, true, false, false, true]
+artists = ["Drake", "Ed Sheeran", "Bruno Mars"]
+colour_syms = [:blue, :red, :green]
+
+
+urban_defs = {"homoblivious" => "Not having the ability to recognize"\
+                                "homosexuals as homosexuals; a lack of gaydar.",
+              "Boobs Mcgee"  => "The nickname given to the girl inthe room"\
+                                "flaunting her chest the most.",
+              "Baklol"       => "A Baklol is a person who is supposed to be"\
+                                "stupid. It is a Bihari word, generally used"\
+                                "by the people of Bihar and Eastern Uttar"\
+                                "Pradesh in India."
+             }
+movies = {
+          "The Wizard of Oz" => 1939,
+          "Citizen Kane"     => 1941,
+          "The Third Man"    => 1949
+         }
+cities = {
+          "Shanghai"         => 24_256_800,
+          "Karachi"          => 23_500_000,
+          "Delhi"            => 16_787_941
+         }
+sis_and_friends = {"Katie" => 35, "Mark" => 27, "Lauren" => 23}
+
+puts flips
+puts colours[0]
+puts ages.sort
+ages << 0
+puts movies["Citizen Kane"]
